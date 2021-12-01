f298260a1c4284f0bbae3d3bc73f0369fc532b59
Alex Rønne Petersen <alex@alexrp.com>
2021-12-01 14:05:50 +0100

    Use Win32Exception.Message in WindowsTerminalUtility.ThrowIfUnexpected.
    Part of #34.

--- src/core/Drivers/Windows/WindowsTerminalUtility.cs
+++ src/core/Drivers/Windows/WindowsTerminalUtility.cs
@@ -29,15 +29,15 @@ static class WindowsTerminalUtility
     public static void ThrowIfUnexpected(string message)
     {
         // TODO: https://github.com/microsoft/CsWin32/issues/452
-        var err = (WIN32_ERROR)Marshal.GetLastSystemError();
+        var err = Marshal.GetLastSystemError();
 
         // See comments in UnixTerminalWriter for the error handling rationale.
-        switch (err)
+        switch ((WIN32_ERROR)err)
         {
             case WIN32_ERROR.ERROR_HANDLE_EOF or WIN32_ERROR.ERROR_BROKEN_PIPE or WIN32_ERROR.ERROR_NO_DATA:
                 break;
             default:
-                throw new TerminalException($"{message}: {err}");
+                throw new TerminalException($"{message}: {new Win32Exception(err).Message}");
         }
     }
 }
