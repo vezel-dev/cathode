743975d3eac4c2f1fb64d24dddc0fa7dd7044a7d
Alex Rønne Petersen <alex@alexrp.com>
2021-12-06 00:37:34 +0100

    Rename some P/Invoke parameters to match POSIX.

--- src/core/Unix/MacOS/MacOSPInvoke.cs
+++ src/core/Unix/MacOS/MacOSPInvoke.cs
@@ -7,8 +7,8 @@ static unsafe class MacOSPInvoke
     public static extern int poll(pollfd* fds, uint nfds, int timeout);
 
     [DllImport("c", SetLastError = true)]
-    public static extern int tcgetattr(int fd, out termios termios_p);
+    public static extern int tcgetattr(int fildes, out termios termios_p);
 
     [DllImport("c", SetLastError = true)]
-    public static extern int tcsetattr(int fd, int optional_actions, in termios termios_p);
+    public static extern int tcsetattr(int fildes, int optional_actions, in termios termios_p);
 }
--- src/core/Unix/UnixPInvoke.cs
+++ src/core/Unix/UnixPInvoke.cs
@@ -4,20 +4,20 @@ namespace System.Unix;
 static unsafe class UnixPInvoke
 {
     [DllImport("c", SetLastError = true)]
-    public static extern int isatty(int fd);
+    public static extern int isatty(int fildes);
 
     [DllImport("c", CharSet = CharSet.Ansi, SetLastError = true)]
     [SuppressMessage("Globalization", "CA2101")] // TODO: https://github.com/dotnet/roslyn-analyzers/issues/5479
-    public static extern int open(string pathname, int flags);
+    public static extern int open(string path, int oflag);
 
     [DllImport("c", SetLastError = true)]
-    public static extern nint read(int fd, void* buf, nuint count);
+    public static extern nint read(int fildes, void* buf, nuint nbyte);
 
     [DllImport("c", SetLastError = true)]
-    public static extern nint write(int fd, void* buf, nuint count);
+    public static extern nint write(int fildes, void* buf, nuint nbyte);
 
     [DllImport("c", SetLastError = true)]
-    public static extern int ioctl(int fd, nuint request, out winsize argp);
+    public static extern int ioctl(int fildes, nuint request, out winsize argp);
 
     [DllImport("c", SetLastError = true)]
     public static extern int kill(int pid, int sig);
