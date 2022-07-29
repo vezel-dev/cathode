32ab4f5285cfb0678bb9e4878b2b6fcacbf2f84e
Alex Rønne Petersen <alex@alexrp.com>
2022-07-29 12:11:55 +0200

    Convert EntryPointGenerator to use the incremental source generator APIs.

--- src/analyzers/Hosting/EntryPointGenerator.cs
+++ src/analyzers/Hosting/EntryPointGenerator.cs
@@ -1,69 +1,72 @@
 namespace Vezel.Cathode.Analyzers.Hosting;
 
-[Generator(LanguageNames.CSharp)]
-public sealed class EntryPointGenerator : ISourceGenerator
+[Generator]
+public sealed class EntryPointGenerator : IIncrementalGenerator
 {
-    private sealed class ProgramTypeSyntaxReceiver : ISyntaxContextReceiver
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        public ImmutableArray<INamedTypeSymbol> ProgramSymbols { get; private set; } =
-            ImmutableArray<INamedTypeSymbol>.Empty;
-
-        private INamedTypeSymbol? _interface;
-
-        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
-        {
-            var sema = context.SemanticModel;
-
-            _interface ??= sema.Compilation.GetTypeByMetadataName("Vezel.Cathode.Hosting.IProgram");
-
-            if (context.Node is TypeDeclarationSyntax type)
-                if (sema.GetDeclaredSymbol(type) is INamedTypeSymbol sym)
-                    if (sym.AllInterfaces.Any(sym => sym.Equals(_interface, SymbolEqualityComparer.Default)))
-                        ProgramSymbols = ProgramSymbols.Add(sym);
-        }
-    }
-
-    public void Initialize(GeneratorInitializationContext context)
-    {
-        context.RegisterForSyntaxNotifications(() => new ProgramTypeSyntaxReceiver());
-    }
-
-    public void Execute(GeneratorExecutionContext context)
-    {
-        var syms = ((ProgramTypeSyntaxReceiver)context.SyntaxContextReceiver!).ProgramSymbols;
-
-        // Is the project using the terminal hosting APIs?
-        if (syms.IsEmpty)
-            return;
-
-        if (syms.Length != 1)
-            foreach (var program in syms)
-                foreach (var loc in program.Locations)
-                    context.ReportDiagnostic(
-                        Diagnostic.Create(DiagnosticDescriptors.AvoidMultipleProgramTypes, loc));
-
-        if (context.Compilation.GetEntryPoint(context.CancellationToken) is IMethodSymbol entry)
-        {
-            foreach (var loc in entry.Locations)
-                context.ReportDiagnostic(
-                    Diagnostic.Create(DiagnosticDescriptors.AvoidImplementingEntryPoint, loc, entry));
-
-            return;
-        }
-
-        var name = syms[0].ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-
-        context.AddSource(
-            "GeneratedProgram.g.cs",
-            $$"""
-            [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]
-            static class GeneratedProgram
+        context.RegisterSourceOutput(
+            context.CompilationProvider
+                .Select(static (c, ct) => c.GetEntryPoint(ct))
+                .Combine(
+                    context.SyntaxProvider
+                        .CreateSyntaxProvider(
+                            static (node, _) => node is TypeDeclarationSyntax,
+                            static (ctx, _) => (Node: (TypeDeclarationSyntax)ctx.Node, Model: ctx.SemanticModel))
+                        .Combine(
+                            context.MetadataReferencesProvider
+                                .Combine(context.CompilationProvider)
+                                .Select(static (tup, _) => tup.Right.GetAssemblyOrModuleSymbol(tup.Left))
+                                .Where(static sym => sym is IAssemblySymbol { Name: "Vezel.Cathode.Hosting" })
+                                .Collect()
+                                .Select(static (arr, _) =>
+                                    ((IAssemblySymbol?)arr.FirstOrDefault())?.GetTypeByMetadataName(
+                                        "Vezel.Cathode.Hosting.IProgram")))
+                        .Select(static (tup, ct) =>
+                            (Interface: tup.Right, Candidate: tup.Left.Model.GetDeclaredSymbol(tup.Left.Node, ct)))
+                        .Where(static tup =>
+                            tup is (not null, not null) &&
+                            tup.Candidate.AllInterfaces.Any(iface =>
+                                iface.Equals(tup.Interface, SymbolEqualityComparer.Default)))
+                        .Select(static (tup, _) => tup.Candidate)
+                        .Collect()),
+            static (ctx, tup) =>
             {
-                static global::System.Threading.Tasks.Task Main(string[] args)
+                var syms = tup.Right;
+
+                // Is the project using the terminal hosting APIs?
+                if (syms.IsEmpty)
+                    return;
+
+                if (syms.Length != 1)
+                    foreach (var sym in syms)
+                        foreach (var loc in sym!.Locations)
+                            ctx.ReportDiagnostic(
+                                Diagnostic.Create(DiagnosticDescriptors.AvoidMultipleProgramTypes, loc));
+
+                if (tup.Left is IMethodSymbol entry)
                 {
-                    return global::Vezel.Cathode.Hosting.ProgramHost.RunAsync<{{name}}>(args);
+                    foreach (var loc in entry.Locations)
+                        ctx.ReportDiagnostic(
+                            Diagnostic.Create(DiagnosticDescriptors.AvoidImplementingEntryPoint, loc, entry));
+
+                    return;
                 }
-            }
-            """);
+
+                var name = syms[0]!.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+                ctx.AddSource(
+                    "GeneratedProgram.g.cs",
+                    $$"""
+                    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+                    static class GeneratedProgram
+                    {
+                        static global::System.Threading.Tasks.Task Main(string[] args)
+                        {
+                            return global::Vezel.Cathode.Hosting.ProgramHost.RunAsync<{{name}}>(args);
+                        }
+                    }
+                    """);
+            });
     }
 }
