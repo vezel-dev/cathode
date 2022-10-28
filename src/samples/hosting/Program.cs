52ef48929109fcb5d6dd019547495f80ae289ffa
Alex Rønne Petersen <alex@alexrp.com>
2022-10-28 17:51:34 +0200

    Remove ProgramContext.CancellationToken sample code since it breaks dotnet-example in CI.

--- src/samples/hosting/Program.cs
+++ src/samples/hosting/Program.cs
@@ -6,32 +6,6 @@ internal sealed class Program : IProgram
         context.UnhandledException += e => OutLine($"Unhandled exception event: {e}");
 
         await OutLineAsync($"Arguments: {string.Join(' ', context.Arguments.ToArray())}");
-
-        // Signals cannot be synthesized on Windows.
-        if (!OperatingSystem.IsWindows())
-        {
-            await OutLineAsync("Generating a cancellation signal...");
-
-            // Retrieve the token so that the event handler is wired up.
-            var ct = context.CancellationToken;
-
-            _ = Task.Run(async () =>
-            {
-                await Task.Delay(TimeSpan.FromSeconds(5));
-
-                GenerateSignal(TerminalSignal.Interrupt);
-            });
-
-            try
-            {
-                await Task.Delay(Timeout.InfiniteTimeSpan, ct);
-            }
-            catch (TaskCanceledException)
-            {
-                await OutLineAsync("Caught a cancellation signal.");
-            }
-        }
-
         await OutLineAsync("Switching to raw mode...");
 
         try
